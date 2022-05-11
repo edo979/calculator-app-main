05e038381b6e8cac7545ced2d731886b8d93e84a
edo979 <prezentabilan@gmail.com>
2022-05-11 11:40:59 +0200

    finish change theme

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-const switcherEl = document.querySelector('.theme_switcher')
+const switcherEl = document.querySelector('.theme_switcher'),
+  themeNames = ['light', 'dark', 'violet']
+
+// detect color scheme
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  setActivThemeSwitch(switcherEl.children[1])
+}
+
+// event listenerts
 
 switcherEl.addEventListener('click', (e) => {
   if (e.target.classList.contains('theme_switch')) {
-    Array.from(switcherEl.children).forEach((btn) =>
-      btn.classList.remove('theme_switch--active')
-    )
+    setActivThemeSwitch(e.target)
 
-    e.target.classList.add('theme_switch--active')
+    changeTheme(parseInt(e.target.dataset.switch))
   }
 })
+
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', (event) => {
+    const newColorScheme = event.matches ? 'dark' : 'light'
+
+    if (newColorScheme == 'dark') {
+      setActivThemeSwitch(switcherEl.children[1])
+    } else {
+      setActivThemeSwitch(switcherEl.children[0])
+    }
+  })
+
+// functions for changing theme
+
+function changeTheme(themeNumber) {
+  document.querySelector('body').classList = themeNames[themeNumber - 1]
+}
+
+function setActivThemeSwitch(switchEl) {
+  Array.from(switcherEl.children).forEach((btn) =>
+    btn.classList.remove('theme_switch--active')
+  )
+
+  switchEl.classList.add('theme_switch--active')
+}
